f3f8b1f281f3b4071b5a169a498a64bfa5571e69
Ogajohne <ashiekaa.john@yahoo.com>
2023-06-01 16:55:53 +0100

    Assignment Submitted

---
+++ Aircondition.java
@@ -0,0 +1,32 @@
+public class Aircondition{
+
+private boolean isOn;
+private string productName;
+private int temperature;
+
+public Aircondiion(string productName) { 
+}
+public void decreaseTemperature() {
+}
+public string getProductName() {
+return productName;
+}
+public int getTemperature() {
+return temperature;
+}
+public void increaseTemperature() {
+}
+public boolean isOn() {
+return isOn;
+}
+public void setOn(boolean isOn) {
+this.isOn=isOn;
+}
+public void setProductName(string productName) {
+ this.productName=productName;
+ }
+public void setTemperature(int temperature) {
+ this.temperature=te=temperature;
+ }
+
+}
